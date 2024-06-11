330b8c4234a14166aa766991b5367246afe5502c
SacredSqueegee <16947007+SacredSqueegee@users.noreply.github.com>
2024-06-11 10:12:46 -0500

    Added previous function files and base main file

--- sum-of-squares/src/main.s
+++ sum-of-squares/src/main.s
@@ -1,14 +1,21 @@
+// Sum of Squares
+// 
+// Description:
+//      This program takes a given input array of bytes, square each
+//      entry, then sum all entries together for the final result
+//
+
+
+
 .section .text
 
 .global _start
 _start:
 
 print_hello_world:
-    mov R0, #1                      @ 1 = stdout
-    ldr R1, =hello_world            @ str pointer
-    mov R2, #hello_world_size       @ str len
-    mov R7, #4                      @ linux write syscall
-    svc 0                           @ software interrupt call write
+
+
+
 
 exit:
     mov R0, #0                      @ return code
@@ -16,6 +23,8 @@ exit:
     svc 0                           @ software interrupt call exit
 
 .section .data
-hello_world:    .ascii "Hello World!\n"
-                .set hello_world_size, .-hello_world    // Set assembler immediate
-
+// Out input array of bytes to square and then sum
+inarr:      .byte   65,71,71,71,67,72,66,51,58,50
+            .byte   60,60,55,64,52,69,52,67,55,69,0
+            .set inarr_size, .-inarr
+outstr:     .fill   11
