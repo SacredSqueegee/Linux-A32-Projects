13e60820bc7e11873331fe54a4d43b63a61cf050
SacredSqueegee <16947007+SacredSqueegee@users.noreply.github.com>
2024-06-10 20:32:47 -0500

    Refactored itoa function
    Refactored itoa function so that it makes more streamlined use of the new pow function and doesn't have a lot of ugly boiler plate and bloat wrapped around the pow function call. In redoing the itoa function I also refrained from using regesters 0-3 for persistant storage to be more in line with the linux arm32 abi. That said, the file needs to be cleaned up again and tested more before moving on.

--- factorials/src/itoa.s
+++ factorials/src/itoa.s
@@ -44,12 +44,13 @@ itoa:
                             // SP must be two times the pointer size, i.e. 8-byte aligned
 
     str r0, [fp, #-4]       // Store output buffer address on stack as local var
+    mov r4, r1              // r4 = #
 
 
     // Prepare for conversion
     push {r2-r7}            // Save 6 registers -> 48-bytes, to the stack that will be modified
-    mov r2, #0              // r2 = x = 0
-                            // r3 = y
+    mov r5, #0              // r5 = x = 0
+                            // r6 = y
 
 
     // TODO: use a different register for doing our 'r5 = (10^x) * 9' operation
@@ -60,104 +61,65 @@ itoa:
     // we can decrment 'x' till done in our loop_2 below
     loop_1:
         // compute (9 * 10^x)
-        mov r6, #10         // base = 10
-        mov r7, r2          // exp = x
+        mov r0, #10         // base = 10
+        mov r1, r5          // exp = x
+        bl pow              // 10^x
 
-        //pow r5, r6, r7      // r5 = 10 ^ x
-        // WARN: ugly use of pow, redo itoa() to accomidate pow being a function
-        //       instead of a macro now.
-            push {r0-r1}
-            mov r0, r6
-            mov r1, r7
-            bl pow
-            mov r5, r0
-            pop {r0-r1}
-
-        mov r6, #9
-        mul r5, r6          // r5 = (10^x) * 9
+        mov r1, #9
+        mul r0, r1          // r0 = (10^x) * 9
 
         // Do comparison of # and (9 * 10^x)
-        cmp r1, r5
+        cmp r4, r0
         ble loop_1_done     // if (# <= (9 * 10^x)) -> goto loop_1_done
                             // else -> go on below
-        add r2, #1          // inc x
+        add r5, #1          // inc x
         b loop_1            // repeat
 
     
     loop_1_done:
-        mov r3, #0          // r3 = y = 0
-        mov r6, #10         // r6 -> base = 10; Does not change from here on
+        mov r6, #0          // r6 = y = 0
+        mov r7, #0          // r6 = temp = 0
 
 
     // Find y so that [# >= (y * 10^x)]
     loop_2:
         // compute y * 10^x
-        //mov r6, #10         // base = 10
-        // r6 set at loop_1_done, and is not modified
-        mov r7, r2          // exp = x
-        //pow r5, r6, r7      // r5 = 10 ^ x
-
-        // WARN: ugly use of pow, redo itoa() to accomidate pow being a function
-        //       instead of a macro now.
-            push {r0-r1}
-            mov r0, r6
-            mov r1, r7
-            bl pow
-            mov r5, r0
-            pop {r0-r1}
-
-        mul r5, r3          // r5 = (10^x) * y
+        mov r0, #10         // base = 10
+        mov r1, r5          // exp = x
+        bl pow              // r0 = 10^x
+        mul r0, r6          // r0 = (10^x) * y
 
         // Do comparison of # and (y * 10^x)
-        cmp r1, r5
+        cmp r4, r0
         blt loop_2_done     // if (# < (9 * 10^x)) -> goto loop_2_done
                             // else -> go on below
-        add r3, #1          // inc x
+        add r6, #1          // inc y
+        mov r7, r0          // save prev. (y*10^x) computation for when we finish
+                            // loop_2 and need the previous value
         b loop_2            // repeat
         
 
     // Convert 'y' to ascii, store, check and prepare for next loop if needed
     loop_2_done:
-        sub r3, #1          // When loop_2 finishes we overshoot the number by 1
+        sub r6, #1          // When loop_2 finishes we overshoot the number by 1
                             // so sub 1 to compensate
 
-        // re-compute y * 10^x with correct y value from above
-        // TODO: could probably store previous (y * 10^x) value from above so we
-        //       don't have to recompute it again here
-
-        //mov r6, #10         // base = 10
-        // r6 set at loop_1_done, and is not modified
-        mov r7, r2          // exp = x
-
-        //pow r5, r6, r7      // r5 = 10 ^ x
-        // WARN: ugly use of pow, redo itoa() to accomidate pow being a function
-        //       instead of a macro now.
-            push {r0-r1}
-            mov r0, r6
-            mov r1, r7
-            bl pow
-            mov r5, r0
-            pop {r0-r1}
-
-        mul r5, r3          // r5 = (10^x) * y
-
-        // compute: # - (y * 10^x)
-        sub r1, r5
+        // update # to: # - (y * 10^x)
+        sub r4, r7
 
         // Store computed 'y' value as ascii char to outstr
-        add r3, #0x30       // convert base-10 to ascii
+        add r6, #0x30       // convert base-10 to ascii
         //strb r3, [r0], #1   // Save char to outstr, inc to next str location
         testing:
         ldr r0, [fp, #-4]
-        strb r3, [r0], #1
+        strb r6, [r0], #1
         str r0, [fp, #-4]
 
         // check if we are done, if not -> prepare for next loop and repeate
-        sub r2, #1          // dec x
-            cmp r2, #0
+        sub r5, #1          // dec x
+            cmp r5, #0
             blt done        // if x < 0 -> goto done
-        mov r3, #0          // y = 0
-        b loop_2            // goto loop_t
+        b loop_1_done       // goto loop_t
 
 
 // NOTE: Leaving done label below just in case we want to do something in the future before
