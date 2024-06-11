2cfb7974987a57da404bcf4c4d0713552e00b1e3
SacredSqueegee <16947007+SacredSqueegee@users.noreply.github.com>
2024-06-11 10:48:53 -0500

    Finished the program
    This one was quite simple and revolves around the mla instruction

--- sum-of-squares/src/main.s
+++ sum-of-squares/src/main.s
@@ -6,15 +6,49 @@
 //
 
 
-
 .section .text
+
+.include "write.s"
 
 .global _start
 _start:
 
 print_hello_world:
 
+    // Registers:
+    // r4 -> addr of inarr
+    // r5 -> addr of outstr
+    // r6 -> accumulated total
+    // r7 -> current array entry / read number
+    // r8 -> loop/remaining index counter
 
+    // Initialize registers
+    ldr r4, =inarr
+    ldr r5, =outstr
+    mov r6, #0
+    mov r8, #inarr_size         // Loop counter, init to size of array
+
+    loop:
+        ldrb r7, [r4], #1       // Load current array value to r1, inc to next index after
+        mla r6, r7, r7, r6      // r0 = (r1 * r1) + r0 = r1^2 + r0
+
+        sub r8, #1              // dec remaining indexes/loop counter
+        cmp r8, #0              // if 0, ther are no more remaining array entries
+        bne loop                // repeat if not 0, we have more array entries
+        
+    
+done:
+    // Convert our answer to ascii
+    mov r0, r5
+    mov r1, r6
+    bl itoa
+
+    // Write our answer and newline to terminal
+    write outstr
+
+    mov r0, #0x000a             
+    strh r0, [r5]               // write '\n', '\0' to outstr
+    write outstr                // write the new line
 
 
 exit:
@@ -28,3 +62,4 @@ inarr:      .byte   65,71,71,71,67,72,66,51,58,50
             .byte   60,60,55,64,52,69,52,67,55,69,0
             .set inarr_size, .-inarr
 outstr:     .fill   11
+
